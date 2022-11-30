cf5ac66999058a615f59db3e68a842541ed578cf
Matt Vaughn <matt.vaughn@buildmotion.com>
2022-11-29 21:39:09 -0700

    update datadog config with new creds

--- apps/black-dashboard/src/config/app-config.ts
+++ apps/black-dashboard/src/config/app-config.ts
@@ -14,7 +14,7 @@ export interface IConfiguration {
 
 export const AppConfig: IConfiguration = {
     apiConfig: {
-        apiURL: 'https://api-dev.bv-bim.com/api',
+        apiURL: 'https://api-dev.dashboard/api',
         baseUrl: '',
         csrf: '',
         health: '',
@@ -22,38 +22,38 @@ export const AppConfig: IConfiguration = {
     },
     dataDogConfig: {
         logs: {
-            clientToken: 'pub905382b0fa24bc2f51efa742260ef110',
+            clientToken: 'pubb4184308df8ee85b01a4a770e4a2dc8a',
             forwardErrorsToLogs: true,
             sampleRate: 4,
-            site: 'bv-bim.com'
+            site: 'Dashboard Demo in Clearwater'
         },
         realUserMonitoring: {
-            applicationId: '55f62565-9fec-48cc-9987-813bb6d1185d',
-            clientToken: 'pubf256b5f05b9a558b62821edc0947b4ec',
+            applicationId: '1ce4038b-543a-43fb-bef4-33945b3e962c',
+            clientToken: 'pubf6c676607c0a8bc7a4d801d5609a8e05',
             site: 'datadoghq.com',
-            service: 'test-app',
+            service: 'dashboard-demo@pwx-2022',
             env: '',
             version: '1.0.0',
             sampleRate: 100,
             premiumSampleRate: 100,
             trackInteractions: true,
-            defaultPrivacyLevel: 'mask-user-input'
+            defaultPrivacyLevel: 'mask-user-input',
         }
     },
     errorHandlingConfig: {
-        applicationName: 'bv-bim.com',
+        applicationName: 'DashboardDemo',
         includeDefaultErrorHandling: true
     },
     loggingConfig: {
-        applicationName: 'bv-bim.com',
+        applicationName: 'DashboardDemo',
         isProduction: false
     },
     webConfig: {
-        applicationName: 'creatr',
+        applicationName: 'Dashboard Demo',
         blogURL: 'https://www.medium.com/@angularlicious',
-        companyName: 'Creatr',
+        companyName: 'DashboardDemo',
         companyEffectiveDate: new Date(2020, 10, 1),
-        email: 'creatr@buildmotion.com',
+        email: 'dashboard@buildmotion.com',
         social: {
             github: {
                 name: 'Angular Workspace (course repository)',
@@ -66,7 +66,7 @@ export const AppConfig: IConfiguration = {
             twitter: { name: '@AngularArch', URL: 'https://twitter.com/AngularArch' },
         },
         version: '',
-        website: 'Creatr',
+        website: 'DashboardDemo',
         defaultPageSize: 25,
         googleTagManagerId: 'GTM-JHHJJH',
         keywordValidationInMilliseconds: 750,
