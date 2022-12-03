8e95e26947dc628a4c4006a10ee2e25b3249c876
Matt Vaughn <matt.vaughn@buildmotion.com>
2022-12-02 23:33:42 -0500

    implement UI Service

--- libs/dashboard/accounts/ui/src/lib/new-account/new-account-ui.service.ts
+++ libs/dashboard/accounts/ui/src/lib/new-account/new-account-ui.service.ts
@@ -1,7 +1,17 @@
 import { Injectable } from '@angular/core';
+import { accountInfo } from '@buildmotion/dashboard-types';
+import { ServiceBase, ServiceContext } from '@buildmotion/foundation';
+import { LoggingService, Severity } from '@buildmotion/logging';
 
-@Injectable({
-  providedIn: 'root'
-})
-export class NewAccountUiService {
+@Injectable()
+export class NewAccountUiService extends ServiceBase {
+
+  constructor(
+    loggingService: LoggingService, serviceContext: ServiceContext) {
+    super('NewAccountUIService', loggingService, serviceContext);
+  }
+
+  createAccount(newAccount: accountInfo.NewAccount) {
+    this.loggingService.log(this.serviceName, Severity.Information, `Preparing to create new account for [${newAccount.emailAddress ?? 'n/a'}]`);
+  }
 }
