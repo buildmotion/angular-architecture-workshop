4f31ec62fb502a5647b253b6f35c37b539a13750
Matt Vaughn <matt.vaughn@buildmotion.com>
2022-12-02 21:10:26 -0500

    implement reactive form [component, module]

--- libs/dashboard/accounts/ui/src/lib/new-account/new-account.component.ts
+++ libs/dashboard/accounts/ui/src/lib/new-account/new-account.component.ts
@@ -1,12 +1,64 @@
 import { Component, OnInit } from '@angular/core';
+import { AbstractControl, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ComponentBase } from '@buildmotion/foundation';
+import { LoggingService, Severity } from '@buildmotion/logging';
 
 @Component({
   selector: 'buildmotion-new-account',
   templateUrl: './new-account.component.html',
-  styleUrls: ['./new-account.component.css'],
+  styleUrls: ['./new-account.component.css']
 })
-export class NewAccountComponent implements OnInit {
-  constructor() {}
+export class NewAccountComponent extends ComponentBase implements OnInit {
 
-  ngOnInit(): void {}
+  form: FormGroup;
+
+  // shared password validators;
+  private passwordValidators = [Validators.required, Validators.minLength(8), Validators.maxLength(128)];
+
+  constructor(
+    private formBuilder: FormBuilder,
+    loggingService: LoggingService,
+    router: Router
+  ) {
+    super('NewAccountComponent', loggingService, router);
+  }
+
+  ngOnInit(): void {
+    this.loggingService.log(this.componentName, Severity.Information, `Preparing to initialize component.`);
+    this.initializeForm();
+  }
+
+  initializeForm() {
+    this.loggingService.log(this.componentName, Severity.Information, `Preparing to initialize the create account form.`);
+    this.form = this.formBuilder.group({
+      emailAddress: new FormControl(undefined, {
+        validators: [Validators.required, Validators.maxLength(100)],
+        asyncValidators: [
+          // ADD ASYNC EMAIL VALIDATION HERE;
+        ],
+        updateOn: 'blur',
+      }),
+      password: new FormControl(undefined, {
+        validators: [...this.passwordValidators],
+        asyncValidators: [],
+      }),
+      passwordConfirm: new FormControl(undefined, {
+        validators: { ...this.passwordValidators },
+        updateOn: 'change',
+      }),
+    });
+  }
+
+  get emailAddress(): AbstractControl {
+    return this.form.get('emailAddress') as FormControl;
+  }
+
+  get passwordConfirm(): AbstractControl {
+    return this.form.get('passwordConfirm') as FormControl;
+  }
+
+  get password(): AbstractControl {
+    return this.form.get('password') as FormControl;
+  }
 }
--- libs/dashboard/accounts/ui/src/lib/new-account/new-account.module.ts
+++ libs/dashboard/accounts/ui/src/lib/new-account/new-account.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
 import { NewAccountComponent } from './new-account.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 const routes: Routes = [{ path: '', component: NewAccountComponent }];
 
 @NgModule({
   declarations: [NewAccountComponent],
-  imports: [CommonModule, RouterModule.forChild(routes)],
+  imports: [CommonModule, RouterModule.forChild(routes), FormsModule,
+    ReactiveFormsModule,],
 })
 export class NewAccountModule {}
