1f6c9c2575f7fecb5d76384dbbf87ce9496ba03d
Matt Vaughn <matt.vaughn@buildmotion.com>
2022-12-03 00:14:07 -0500

    rename to AccountService

--- libs/dashboard/accounts/service/src/lib/accounts-service.service.spec.ts
+++ libs/dashboard/accounts/service/src/lib/accounts-service.service.spec.ts
@@ -1,9 +1,10 @@
 import { TestBed } from '@angular/core/testing';
+import { LoggingService, LoggingServiceMock } from '@buildmotion/logging';
+import { AccountsService } from './accounts-service.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
-import { ServiceService } from './service.service';
-
-describe('ServiceService', () => {
-  let service: ServiceService;
+describe('AccountsService', () => {
+  let service: AccountsService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -15,7 +16,7 @@ describe('ServiceService', () => {
         },
       ],
     });
-    service = TestBed.inject(ServiceService);
+    service = TestBed.inject(AccountsService);
   });
 
   it('should be created', () => {
